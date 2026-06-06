d6efc676b5a06b34ca7021c3c85d5a85d1b836f5
lonelynitz <vasanthmurugesan47@gmail.com>
2026-06-06 11:25:59 +0530

    space

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -22,7 +22,7 @@ export default function Contact() {
     setError('');
 
     try {
-      const webUrl = 'https://script.google.com/macros/s/AKfycbyPHuGFTDQuFQPPiHPJTB8k8MkOnG5n2-tAlxDG0DE7mf1MrxBGJeTsd3fs4Pf2b2NB9A/exec';
+      const webUrl = 'https://script.google.com/macros/s/AKfycbyPHuGFTDQuFQPPiHPJTB8k8MkOnG5n2-tAlxDG0DE7mf1MrxBGJeTsd3fs4Pf2b2NB9A/exec'; 
 
       const response = await fetch(webUrl, {
         method: 'POST',
